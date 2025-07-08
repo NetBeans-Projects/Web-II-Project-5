28a114572e9bb17b7f4ce83a0ae55e521153dd68
Jude <jmalshanmel@gmail.com>
2025-07-08 18:09:29 +0530

    Web II Project 5 commiting completed Day Full Backend Code 2025.07.08

---
+++ src/java/controller/Login.java
@@ -0,0 +1,62 @@
+package controller;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        Gson gson = new Gson();
+        User u = gson.fromJson(request.getReader(), User.class);
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/web_5?useSSL=false",
+                    "root",
+                    "Judemysql@2004");
+            Statement smt = connection.createStatement();
+
+            ResultSet rs = smt.executeQuery("SELECT * FROM `user` WHERE `mobile`='" + u.getMobile() + "' && "
+                    + "`password` = '" + u.getPassword() + "'");
+            
+            if (rs.next()) {
+                
+                User u1 = new User();
+                u1.setFirstName(rs.getString("first_name"));
+                u1.setLastName(rs.getString("last_name"));
+                u1.setMobile(rs.getString("mobile"));
+                u1.setCountry(rs.getString("country"));
+                
+                request.getSession().setAttribute("user", u1);
+                response.getWriter().write("Success");
+                
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+
+            e.printStackTrace();
+            response.getWriter().write("login failed.");
+
+        }
+
+    }
+
+}
