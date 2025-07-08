ffda15299334c4324cd54cbd9b72d16c151af4d2
Jude <jmalshanmel@gmail.com>
2025-07-08 12:48:41 +0530

    Web II Project 5 commiting completed until now

---
+++ src/java/controller/CreateNewAccount.java
@@ -0,0 +1,54 @@
+package controller;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+@WebServlet(name = "CreateNewAccount", urlPatterns = {"/CreateNewAccount"})
+public class CreateNewAccount extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+        // Json -> Java
+        Gson gson = new Gson();
+        User user = gson.fromJson(request.getReader(), User.class);
+
+//        System.out.println(user.getMobile());
+//        System.out.println(user.getFirstName());
+//        System.out.println(user.getLastName());
+//        System.out.println(user.getPassword());
+//        System.out.println(user.getCountry());
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/web_5?useSSL=false", "root", "Judemysql@2004");
+            Statement s = c.createStatement();
+            s.executeUpdate("INSERT INTO "
+                    + "`user` (`mobile`,`first_name`,`last_name`,`password`,`country`) "
+                    + "VALUES('" + user.getMobile() + "','" + user.getFirstName() + "','" + user.getLastName() + "','" + user.getPassword() + "','" + user.getCountry() + "')");
+
+            response.getWriter().write("Success");
+            
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+            
+        }
+
+        response.getWriter().write("Hello");
+
+    }
+
+}
---
+++ src/java/model/User.java
@@ -0,0 +1,56 @@
+
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    
+    private String mobile;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String country;
+    
+    public User() {   }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+}
